c5b597d6f6412cc6ebf0c6b8fc865f36d1da6daa
catupe <mesa.alvaro@gmail.com>
2017-02-15 22:07:26 -0300

    base de datos limpia, sin datos

---
+++ bd.mailing.sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-02-2017 a las 02:06:24
+-- Versión del servidor: 5.6.20
+-- Versión de PHP: 5.5.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `mailing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `campania`
+--
+
+CREATE TABLE IF NOT EXISTS `campania` (
+`id` int(11) NOT NULL,
+  `fecha_alta` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `fecha_fin` timestamp NULL DEFAULT NULL,
+  `activa` smallint(6) NOT NULL DEFAULT '1',
+  `fecha_comienzo_campania` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `fecha_fin_campania` timestamp NULL DEFAULT NULL,
+  `id_cliente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE IF NOT EXISTS `cliente` (
+`id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `telefono` varchar(10) DEFAULT NULL,
+  `activo` smallint(6) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `destinatario`
+--
+
+CREATE TABLE IF NOT EXISTS `destinatario` (
+`id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `item_campania`
+--
+
+CREATE TABLE IF NOT EXISTS `item_campania` (
+  `id_campania` int(11) NOT NULL,
+  `id_destinatario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mailing`
+--
+
+CREATE TABLE IF NOT EXISTS `mailing` (
+`id` int(11) NOT NULL,
+  `asunto` varchar(255) NOT NULL,
+  `cuerpo` varchar(255) NOT NULL,
+  `adjunto` varchar(255) NOT NULL,
+  `id_campania` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `campania`
+--
+ALTER TABLE `campania`
+ ADD PRIMARY KEY (`id`), ADD KEY `id_cliente` (`id_cliente`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `destinatario`
+--
+ALTER TABLE `destinatario`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indices de la tabla `item_campania`
+--
+ALTER TABLE `item_campania`
+ ADD KEY `id_campania` (`id_campania`,`id_destinatario`);
+
+--
+-- Indices de la tabla `mailing`
+--
+ALTER TABLE `mailing`
+ ADD PRIMARY KEY (`id`), ADD KEY `id_campania` (`id_campania`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `campania`
+--
+ALTER TABLE `campania`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `destinatario`
+--
+ALTER TABLE `destinatario`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `mailing`
+--
+ALTER TABLE `mailing`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
